08a0f57eaf3f2409b440dc60d628a9604f652250
lemique17 <lemique17@github.com>
2023-07-20 17:56:56 +0200

    GUI with multiple screens

--- matchedfilter.py
+++ matchedfilter.py
@@ -10,81 +10,42 @@ import sys
 import os
 import templatebank_handler as handler  # selfmade; defines classes for matched filtering
 
+
+### Get defaults
+bankpath_default = 'C:/Users/Praktikum/Desktop/LIGO/template_bank/'
+bankpath_default_2 = "C:/Users/Praktikum/Desktop/LIGO/template_bank"
+
+### General settings
+cwd = os.getcwd()
 debugmode = True
 
-# I guess I could get rid of the "debug-methods"? Or at least change them.
+mpi_path_host = cwd
+mpi_path_container = '/input/mpi/'
+connection = handler.connect()
+### !!!!!!!!!!!!! could be removed in the end:
+if debugmode: 
+	connection.update_mpi(mpi_path_host, mpi_path_container)
 
 
-class SoundofMI(QMainWindow):
+class TemplateScreen(QMainWindow):
 
-	# set defaults
-	cwd = os.getcwd()
-	defaultbankpath = 'C:/Users/Praktikum/Desktop/LIGO/template_bank/'
-
-	def __init__(self):
-	    ### general settings and initiation of ui
-	    super(SoundofMI,self).__init__()
-	    loadUi(SoundofMI.cwd+'/matchedfilter.ui',self)
-	    self.setWindowTitle('Matched Filtering with pycbc')
-	    self.templatebank = handler.TemplateBank()
-	    self.connection = handler.connect()
-	    mpi_path_host = '/home/mic/promotion/f-prakt_material/LIGO/pycbc/MatchedFilter/'
-	    mpi_path_container = '/input/mpi/'
-	    self.connection.update_mpi(mpi_path_host, mpi_path_container)
-	    ## initiate Push Buttons
-	    # 1 input section
-	    self.pushButton_loadsignal.clicked.connect(self.loadsignal)
-	    # 2a Matched Filtering with template bank section
-	    self.pushButton_loadbank.clicked.connect(self.loadbank)
-	    self.pushButton_loadtemplate.clicked.connect(self.loadtemplate)
-	    self.pushButton_tbchangepath.clicked.connect(self.changetbsavepath)
-	    self.pushButton_bank.clicked.connect(self.matchedfilter_templatebank)
-	    # 2b Matched Filtering with single file
-	    self.pushButton_stchangepath.clicked.connect(self.changestsavepath)
-	    self.pushButton_single.clicked.connect(self.matchedfilter_single)
-	    ## initiate feedback for user
-	    if debugmode:
-	    	self.label_showstatus.setText('ready')
-	    	self.show()
-	    	print('<<< ready >>>')
-
-	### debug-methods (feedback for user)
-	def beginmethod(self, name):
-		if debugmode:
-			self.label_showstatus.setText('evaluating')
-			self.label_showtask.setText(name)
-			self.show()
-			print('----------------')
-			print('### >'+name+'< started... ###')
-			print('----')
-		return
-
-	def begintask(self, name):
-		if debugmode:
-			self.label_showstatus.setText('evaluating')
-			self.label_showtask.setText(name)
-			self.show()
-			print('>started: '+name)
-		return
-
-	def endtask(self, name):
-		if debugmode:
-			self.label_showstatus.setText('ready')
-			self.label_showtask.setText('no task')
-			self.show()
-			print('>completed: '+name+'<')
-			print('----')
-		return
-
-	def endmethod(self, name):
-		if debugmode:
-			self.label_showstatus.setText('ready')
-			self.label_showtask.setText('no task')
-			self.show()
-			print('### >'+name+'< succesful. ###')
-			print('----------------')
-			print('<<< ready >>>')
-		return
+	def __init__(self, templatebank, labels=None):
+		# general settings and initiation of ui
+		super(TemplateScreen,self).__init__()
+		loadUi(cwd+'/template_screen.ui',self)
+		self.setWindowTitle('Matched Filtering with pycbc (Template Management)')
+		self.templatebank = templatebank
+		# connect Push Buttons
+		self.pushButton_createTemplates.clicked.connect(self.create_templates)
+		self.pushButton_loadDirectory.clicked.connect(self.load_directory)
+		self.pushButton_loadFile.clicked.connect(self.load_file)
+		self.pushButton_continue.clicked.connect(self.to_signal_screen)
+		# set Status
+		if labels:
+			self.label_TempLine1.setText(labels[0])
+			self.label_TempLine2.setText(labels[1])
+			self.label_TempLine3.setText(labels[2])
+			self.label_TempLine4.setText(labels[3])	
 
 	### methods for file-Dialogs
 	# see: https://pythonspot.com/pyqt5-file-dialog/
@@ -98,7 +59,7 @@ class SoundofMI(QMainWindow):
 	def openFileNamesDialog(self):
 		options = QFileDialog.Options()
 		options |= QFileDialog.DontUseNativeDialog
-		files, _ = QFileDialog.getOpenFileNames(self,"Open Template-File(s)", "C:/Users/Praktikum/Desktop/LIGO/template_bank","All Files (*);;Python Files (*.py)", options=options)
+		files, _ = QFileDialog.getOpenFileNames(self,"Open Template-File(s)", bankpath_default_2,"All Files (*);;Python Files (*.py)", options=options)
 		if files:
 			return files
 
@@ -112,103 +73,138 @@ class SoundofMI(QMainWindow):
 
 	### methods connected with Push Buttons
 	@pyqtSlot()
-	def loadsignal(self):
-		methodname = 'load signal'
-		self.beginmethod(methodname)
-		fullname = self.openFileNameDialog("Open Signal-File", "C:/Users/Praktikum/Desktop/LIGO")
-		path = os.path.dirname(fullname)+'/'
-		filename = os.path.basename(fullname)
-		self.dataobj = handler.Data(path, filename)
-		self.label_sigfile.setText(fullname)
-		self.label_evastatus.setText('Loaded signal: '+filename)
-		self.label_evastatus_2.setText('Loaded signal: '+filename)
-		self.label_tboutpath.setText(self.dataobj.savepath)
-		self.lineEdit_tbcaption.setText('deactivated')
-		self.label_stoutpath.setText(self.dataobj.savepath)
-		self.lineEdit_stcaption.setText('deactivated')
-		self.show()
-		self.endmethod(methodname)
+	def create_templates(self):
+		print('Creating of new templates is not implemented yet.')
 		return
 
 	@pyqtSlot()
-	def loadbank(self):
-		methodname = 'load directory'
-		self.beginmethod(methodname)
-		path = self.getDirectoryDialog("Choose the directory to be loaded.", self.defaultbankpath)
+	def load_directory(self):
+		path = self.getDirectoryDialog("Choose the directory to be loaded.", bankpath_default)
 		self.templatebank.add_directory(path)
-		if not self.label_dir2.text()=='None':
-			self.label_dirmore.setText('and more')
-		self.label_dir2.setText(self.label_dir1.text())
-		self.label_dir1.setText(self.label_dir0.text())
-		self.label_dir0.setText(path)
+		label = self.make_label(path, 48, 8)
+		if not self.label_TempLine3.text()=='None':
+			self.label_TempLine4.setText('and more')
+		self.label_TempLine3.setText(self.label_TempLine2.text())
+		self.label_TempLine2.setText(self.label_TempLine1.text())
+		self.label_TempLine1.setText(label)
 		self.show()
-		self.endmethod(methodname)
 		return
 
 	@pyqtSlot()
-	def loadtemplate(self):
-		methodname = 'load single template'
-		self.beginmethod(methodname)
+	def load_file(self):
 		fullname = self.openFileNamesDialog()[0]
-		print(fullname)
 		path = os.path.dirname(fullname)+'/'
 		filename = os.path.basename(fullname)
 		self.templatebank.add_template(path, filename)
-		if not self.label_tmp2.text()=='None':
-			self.label_tmpmore.setText('and more')
-		self.label_tmp2.setText(self.label_tmp1.text())
-		self.label_tmp1.setText(self.label_tmp0.text())
-		self.label_tmp0.setText(filename)
+		label = self.make_label(fullname, 48, 8)
+		if not self.label_TempLine3.text()=='None':
+			self.label_TempLine4.setText('and more')
+		self.label_TempLine3.setText(self.label_TempLine2.text())
+		self.label_TempLine2.setText(self.label_TempLine1.text())
+		self.label_TempLine1.setText(label)
 		self.show()
-		self.endmethod(methodname)
 		return
 
 	@pyqtSlot()
-	def changetbsavepath(self):
-		methodname = 'change output path'
-		self.beginmethod(methodname)
-		newpath = self.getDirectoryDialog('Choose the new output directory.', self.dataobj.datapath)
-		self.label_tboutpath.setText(newpath)
-		self.show()
-		self.endmethod(methodname)
-		return
+	def to_signal_screen(self):
+		labels = ( self.label_TempLine1.text(), self.label_TempLine2.text(), self.label_TempLine3.text(), self.label_TempLine4.text() )
+		self.main = SignalScreen(self.templatebank, labels)
+		self.main.show()
+		self.close()
 
-	@pyqtSlot()
-	def matchedfilter_templatebank(self):
-		methodname = 'matched filtering with template bank'
-		self.beginmethod(methodname)
-		self.dataobj.set_savepath(self.label_tboutpath.text())
-		# self.dataobj.set_shortname(self.lineEdit_tbcaption.text())  # deactivated
-		self.dataobj.matched_filter_templatebank(self.templatebank, self.connection)
-		self.endmethod(methodname)
-		return
+	### additional methods
+	def make_label(self, string, maxlen, indent):
+		'Cut a string from the start so it does not exceed maxlen, even with dots and indentation.'
+		label = string
+		if len(label) > (maxlen-indent):
+			label = '...'+label[-(maxlen-indent-3):]
+		label = ' '*indent+label
+		return label
 
-	@pyqtSlot()
-	def changestsavepath(self):
-		methodname = 'change output path'
-		self.beginmethod(methodname)
-		newpath = self.getDirectoryDialog('Choose the new output directory.', self.dataobj.datapath)
-		self.label_stoutpath.setText(newpath)
-		self.show()
-		self.endmethod(methodname)
-		return
+class SignalScreen(QMainWindow):
 
+	def __init__(self, templatebank, labels):
+		# general settings and initiation of ui
+		super(SignalScreen,self).__init__()
+		loadUi(cwd+'/signal_screen.ui',self)
+		self.setWindowTitle('Matched Filtering with pycbc')
+		self.templatebank = templatebank
+		# connect Push Buttons
+		self.pushButton_loadSignal.clicked.connect(self.load_signal)
+		self.pushButton_changeOutput.clicked.connect(self.change_output)
+		self.pushButton_go.clicked.connect(self.matched_filter)
+		self.pushButton_back.clicked.connect(self.to_template_screen)
+		# set Status
+		if labels:
+			self.label_TempLine1.setText(labels[0])
+			self.label_TempLine2.setText(labels[1])
+			self.label_TempLine3.setText(labels[2])
+			self.label_TempLine4.setText(labels[3])		
+
+	### methods for file-Dialogs
+	# see: https://pythonspot.com/pyqt5-file-dialog/
+	def openFileNameDialog(self, DialogName, defaultpath):
+		options = QFileDialog.Options()
+		options |= QFileDialog.DontUseNativeDialog
+		fileName, _ = QFileDialog.getOpenFileName(self, DialogName, defaultpath ,"All Files (*);;Python Files (*.py)", options=options)
+		if fileName:
+			return fileName
+
+	def openFileNamesDialog(self):
+		options = QFileDialog.Options()
+		options |= QFileDialog.DontUseNativeDialog
+		files, _ = QFileDialog.getOpenFileNames(self,"Open Template-File(s)", bankpath_default_2,"All Files (*);;Python Files (*.py)", options=options)
+		if files:
+			return files
+
+	def getDirectoryDialog(self, DialogName, defaultpath):
+		options = QFileDialog.Options()
+		options |= QFileDialog.DontUseNativeDialog
+		options |= QFileDialog.ShowDirsOnly
+		dirName = QFileDialog.getExistingDirectory(self, DialogName, defaultpath, options=options)+'/'
+		if dirName:
+			return dirName
+
+	### methods connected with Push Buttons
 	@pyqtSlot()
-	def matchedfilter_single(self):
-		methodname = 'matched filtering with single template'
-		self.beginmethod(methodname)
-		fullname = self.openFileNameDialog('Choose Template File.', self.defaultbankpath)
+	def load_signal(self):
+		fullname = self.openFileNameDialog("Open Signal-File", "C:/Users/Praktikum/Desktop/LIGO")
 		path = os.path.dirname(fullname)+'/'
 		filename = os.path.basename(fullname)
-		taskname = 'load template ('+filename+')'
-		self.begintask(taskname)
-		self.tmpobj = handler.Template(path, filename)
-		self.endtask(taskname)
-		self.dataobj.set_savepath(self.label_stoutpath.text())
-		# self.dataobj.set_shortname(self.lineEdit_stcaption.text())  # deactivated
-		self.dataobj.matched_filter_single(self.tmpobj, self.connection)
-		self.endmethod(methodname)
+		self.data = handler.Data(path, filename)
+		self.label_Signal.setText( self.make_label(fullname, 35, 2) )
+		self.label_Output.setText( self.make_label(self.data.savepath,35,2) )
+		self.show()
 		return
+
+	@pyqtSlot()
+	def change_output(self):
+		newpath = self.getDirectoryDialog('Choose the new output directory.', self.data.datapath)
+		self.data.set_savepath(newpath)
+		self.label_Output.setText( self.make_label(self.data.savepath,35,2) )
+		self.show()
+		return
+
+	@pyqtSlot()
+	def matched_filter(self):
+		self.data.matched_filter_templatebank(self.templatebank, connection)
+		return
+
+	@pyqtSlot()
+	def to_template_screen(self):
+		labels = ( self.label_TempLine1.text(), self.label_TempLine2.text(), self.label_TempLine3.text(), self.label_TempLine4.text() )
+		self.main = TemplateScreen(self.templatebank, labels)
+		self.main.show()
+		self.close()
+
+	### additional methods
+	def make_label(self, string, maxlen, indent):
+		'Cut a string from the start so it does not exceed maxlen, even with dots and indentation.'
+		label = string
+		if len(label) > (maxlen-indent):
+			label = '...'+label[-(maxlen-indent-3):]
+		label = ' '*indent+label
+		return label
 
 
 # open Window
@@ -219,6 +215,7 @@ with open('matchedfilter.qss','r') as qss:
 
 app = QApplication(sys.argv)
 app.setStyleSheet(style)
-win = SoundofMI()
+templatebank = handler.TemplateBank()
+win = TemplateScreen(templatebank)
 win.show()
 sys.exit(app.exec_())
