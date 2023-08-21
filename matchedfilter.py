36abaefa7eebf799bd281edf25895975ace30b89
lemique17 <lemique17@github.com>
2023-08-21 14:24:08 +0200

    minor changes

--- matchedfilter.py
+++ matchedfilter.py
@@ -2,7 +2,7 @@
 
 # This is the main-File.
 
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QButtonGroup
 from PyQt5.uic import loadUi
 
--- templatebank_handler.py
+++ templatebank_handler.py
@@ -3,7 +3,6 @@
 import os
 import glob
 import numpy as np
-import pycbc.types as types
 
 ### Select OS and establish a connection object to communicate to mpi
 
